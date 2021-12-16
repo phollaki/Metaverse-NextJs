5c4809ee264a11f7423f3ddb25c66ec85b5eb860
phollaki <phollakos15@gmail.com>
2021-12-16 13:41:20 +0100

    homepage update

--- pages/index.js
+++ pages/index.js
@@ -1,13 +1,34 @@
 import Login from "../components/Login";
 import { useMoralis } from "react-moralis";
+import bg from "../public/cyberpunk.gif";
+import Image from "next/image";
+import profileImage from "../public/cyberpunk2.jpg";
+
 export default function Home() {
   const { isAuthenticated, logout } = useMoralis();
 
   if (!isAuthenticated) return <Login />;
 
   return (
-    <div className="h-screen ">
-      <button onClick={logout}>Logout</button>
+    <div className="h-screen">
+      <div className="flex flex-col absolute z-50 h-full w-full items-center justify-center space-y-4 bg-[rgba(0,0,0,0.7)]">
+        <Image
+          className="object-cover rounded-full"
+          src={profileImage}
+          alt=""
+          height={200}
+          width={200}
+        />
+        <button
+          className="bg-black rounded-lg p-5 font-bold text-xl text-pink-500 font-press"
+          onClick={logout}
+        >
+          Logout
+        </button>
+      </div>
+      <div className="w-full h-screen">
+        <Image src={bg} alt="" layout="fill" objectFit="cover" />
+      </div>
     </div>
   );
 }
