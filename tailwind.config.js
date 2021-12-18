c5e7511530cb5ca8879fbb688f1625708602de4a
phollaki <phollakos15@gmail.com>
2021-12-18 12:17:11 +0100

    optimize dev and production look

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,9 @@
 module.exports = {
-  content: [
+  purge: [
     "./pages/**/*.{js,ts,jsx,tsx}",
     "./components/**/*.{js,ts,jsx,tsx}",
+    "./layout/**/*.{js,ts,jsx,tsx}",
+    "./helpers/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
     extend: {
